429a6036b5457732f5c63f0f92b34de1760d39a3
greti <greticani@yahoo.com>
2022-09-04 15:58:02 +0200

    add webclient configuration on professor service

--- professor-service/src/main/java/com/professor/configuration/SecurityConfig.java
+++ professor-service/src/main/java/com/professor/configuration/SecurityConfig.java
@@ -3,7 +3,15 @@ package com.professor.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.reactive.function.client.WebClient;
 
 
 @EnableWebSecurity
@@ -17,6 +25,37 @@ public class SecurityConfig {
 				.and().oauth2ResourceServer()
 				.jwt();
 		return http.build();
+	}
+
+
+	@Bean
+	WebClient webClient(OAuth2AuthorizedClientManager authorizedClientManager) {
+		ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2Client =
+				new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+		oauth2Client.setDefaultClientRegistrationId("ikub-client");
+		return WebClient.builder()
+				.apply(oauth2Client.oauth2Configuration())
+				.build();
+	}
+
+
+
+	@Bean
+	OAuth2AuthorizedClientManager authorizedClientManager(
+			ClientRegistrationRepository clientRegistrationRepository,
+			OAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+		OAuth2AuthorizedClientProvider authorizedClientProvider =
+				OAuth2AuthorizedClientProviderBuilder.builder()
+						.authorizationCode()
+						.refreshToken()
+						.clientCredentials()
+						.build();
+		DefaultOAuth2AuthorizedClientManager authorizedClientManager = new DefaultOAuth2AuthorizedClientManager(
+				clientRegistrationRepository, authorizedClientRepository);
+		authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+		return authorizedClientManager;
 	}
 
 }
--- professor-service/src/main/java/com/professor/controller/ProfessorController.java
+++ professor-service/src/main/java/com/professor/controller/ProfessorController.java
@@ -1,13 +1,38 @@
 package com.professor.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
+@RequiredArgsConstructor
 @RestController
+@RequestMapping("/professor")
 public class ProfessorController {
+
+    @Value("${courseService.schema}")
+    private String schema;
+    @Value("${courseService.host}")
+    private String host;
+    @Value("${courseService.port}")
+    private Integer port;
+    @Value("${courseService.path}")
+    private String path;
+
+    private final WebClient webClient;
 
     @GetMapping("/ok")
     public String ok(){
-        return "professor-service ok";
+        return "professor-service ok"+webClient.get()
+                .uri(uriBuilder -> uriBuilder.scheme(schema)
+                        .host(host)
+                        .port(port)
+                        .path(path)
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
     }
 }
