54bbe711ed25078b26f35a282f38d56f783e3b02
greti <greticani@yahoo.com>
2022-09-09 20:42:18 +0200

    add filebead config in docker-compose.yml file

--- configuration-server/src/main/java/com/configuration/ConfigurationServerApplication.java
+++ configuration-server/src/main/java/com/configuration/ConfigurationServerApplication.java
@@ -1,20 +1,10 @@
 package com.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.core.io.InputStreamSource;
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.util.ResourceUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Arrays;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
